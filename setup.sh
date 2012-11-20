3ab0509fe668faa40efd4b42447f30c3e2edd8e4
Keith Barrette <etterrab@gmail.com>
2012-11-19 21:05:52 -0500

    Basic setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Pull latest
+echo "Pulling latest dotvim"
+git pull
+
+# Set up Vundle
+if [ ! -d "bundle/vundle" ]; then
+  echo "Cloning Vundle"
+  git clone https://github.com/gmarik/vundle.git bundle/vundle
+fi
+
+echo "Running BundleInstall"
+vim +BundleInstall! +qall 2>/dev/null
+
+echo "Done."
+
